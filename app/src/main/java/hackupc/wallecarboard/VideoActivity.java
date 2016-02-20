7484e62c74116f6de5541b988a6b260b024ef6d5
Duffman <pautenavidal@gmail.com>
2016-02-20 03:47:51 +0100

    video fet, afegides funcions de configuracio de sensors

--- app/src/main/java/hackupc/wallecarboard/VideoActivity.java
+++ app/src/main/java/hackupc/wallecarboard/VideoActivity.java
@@ -1,6 +1,9 @@
 package hackupc.wallecarboard;
 
 import android.graphics.PixelFormat;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,7 +13,7 @@ import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity implements SensorEventListener{
     private final static String TAG="VideoActivity";
     private static final String VIDEO_URL = "http://10.192.114.44:8000/video3gp.3gp";
 
@@ -23,9 +26,19 @@ public class VideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video);
         Log.d(TAG, "onCreate");
         hideSystemUI();
+        configureSensors();
+        configureVideo();
+
+        playVideoLeft();
+    }
+
+    private void configureSensors(){
+        //TODO: Configure sensors
+    }
+
+    private void configureVideo(){
         videoViewLeft = (VideoView) findViewById(R.id.video_view_left);
         videoViewRight = (VideoView) findViewById(R.id.video_view_right);
-        playVideoLeft();
     }
 
     private void playVideoLeft(){
@@ -108,5 +121,15 @@ public class VideoActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE;
 
         getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //TODO: Work with sensors
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //TODO:Work with sensors
     }
 }
